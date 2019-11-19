39d7e18f83951bd17a7830fec8a9b0c64adac531
matejajasak <55713211+matejajasak@users.noreply.github.com>
2019-11-19 21:13:03 +0100

    Add files via upload

---
+++ AlgebraBlog/app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
+public function show($id){
+
+    $post = Post::find($id);
+
+    return view('posts.show', compact('post'));
+    }
+}
